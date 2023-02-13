9e1d658990b743c1910490253887a31d722103a6
argendo <sinderelka43@gmail.com>
2023-02-13 17:22:46 +0300

    surname_and_try_execpt

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['UPLOAD_FOLDER'] = 'static/files'
 app.config['RECAPTCHA_PUBLIC_KEY'] = PUB_CAP
 app.config['RECAPTCHA_PRIVATE_KEY'] = SEC_CAP
 
-allowed_extensions = ['mp3', 'wav', 'mp4', 'mov', 'pptx', 'docx', 'jpg', 'png', 'heic']
+allowed_extensions = ['mp3', 'wav', 'mp4', 'pptx', 'docx', 'jpg', 'png', 'heic']
 
 class UploadFileForm(FlaskForm):
     surname = StringField("Фамилия")
@@ -34,30 +34,54 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
-        fsurname = form.surname.data
+        fsurname = str(form.surname.data).strip()
+        # if good filetype
         if filetype.guess(file).extension != '' and filetype.guess(file).extension in allowed_extensions:
             y = yadisk.YaDisk(token=TOKEN)
             filelist=list(y.listdir('app:/'))
             sflist = [sub['name'] for sub in filelist]
+            # check if surname
             if len(str(fsurname))!=0:
+                # check if there is dir for that surname
                 if len([ sub for sub in filelist if sub['name']==str(fsurname) and sub['type']=='dir']) > 0:
+                    # check if there is same file and save again
                     if str(file.filename) in [sub ['name'] for sub in list(y.listdir('app:/'+str(fsurname)))]:
-                        y.upload(file, 'app:/'+str(fsurname)+'/'+str(file.filename[:-4])+' '+str(int(time.time()))+str(file.filename[-4:]))
-                        args='Такое уже есть в твоей папке, но мы сохранили снова, не благодари ;)'
+                        try:
+                            y.upload(file, 'app:/'+str(fsurname)+'/'+str(file.filename[:-4])+' '+str(int(time.time()))+str(file.filename[-4:]))
+                            args='Такое уже есть в твоей папке, но мы сохранили снова, не благодари ;)'
+                        except:
+                            args="Ничего не вышло, пожалуйста, попробуй снова."
+                    # if new file 
                     else:
+                        try:
+                            y.upload(file, 'app:/'+str(fsurname)+'/'+str(file.filename))
+                            args='Сохранили в твою папку ;)'
+                        except:
+                            args="Ничего не вышло, пожалуйста, попробуй снова."
+                # make dir for surname and save file
+                else:
+                    try:
+                        y.mkdir('app:/'+str(fsurname))
                         y.upload(file, 'app:/'+str(fsurname)+'/'+str(file.filename))
-                        args='Сохранили в твою папку ;)'
-                else:
-                    y.mkdir('app:/'+str(fsurname))
-                    y.upload(file, 'app:/'+str(fsurname)+'/'+str(file.filename))
-                    args="Создали твою папку и сохранили твой файл. Было сложно, но мы справились"
+                        args="Создали твою папку и сохранили твой файл. Было сложно, но мы справились"
+                    except:
+                        args="Ничего не вышло, пожалуйста, попробуй снова."
+            # if no surname 
             else:
+                # check if there is same file and save it
                 if str(file.filename) in sflist:
-                    y.upload(file, 'app:/'+str(file.filename[:-4])+' '+str(int(time.time()))+str(file.filename[-4:]))
-                    args='У нас такое уже есть, но мы сохранили снова, не благодари ;)'
+                    try:
+                        y.upload(file, 'app:/'+str(file.filename[:-4])+' '+str(int(time.time()))+str(file.filename[-4:]))
+                        args='У нас такое уже есть, но мы сохранили снова, не благодари ;)'
+                    except:
+                        args="Ничего не вышло, пожалуйста, попробуй снова."
+                # save new file without surname
                 else:
-                    y.upload(file, 'app:/'+str(file.filename))
-                    args="Всё сохранили, но в общую папку"
+                    try:
+                        y.upload(file, 'app:/'+str(file.filename))
+                        args="Всё сохранили, но в общую папку"
+                    except:
+                        args="Ничего не вышло, пожалуйста, попробуй снова."
         else:
             args="Ничего не вышло, возможно ты загрузил(а) файл не того формата."
     return render_template('index.html', form=form, args=args)
